4b5b19d048f24cd9f134ed019c08428558623061
AlhemyD <AlhemyD@mail.ru>
2024-03-30 10:41:34 +0000

    Реализован запрос на выбор возвраста

--- old_file_remover.sh
+++ old_file_remover.sh
@@ -2,7 +2,7 @@
 
 # Define the directory to search in and the days threshold
 directory=$1
-days_threshold=30
+days_threshold=$2
 
 # Store the output of the find command in a variable
 found_paths=$(find "$directory" -type f -mtime +$days_threshold)
